df138fa9736c67398139601e28b90ca6be172685
maher test <daf1984.douzi@gmail.com>
2024-05-17 21:03:06 +0100

    First initialize the API fetch project

---
+++ main.js
@@ -0,0 +1,29 @@
+// First way(fetch method)
+/*
+fetch("https://jsonplaceholder.typicode.com/users").then((response)=> {response
+    .json()
+    .then((data) => data.map((user) => console.log(user.name,user.address)))
+    .catch((error)=>console.error("Error 404"))
+})
+*/
+
+//Second way(asynchrounos function)
+/*
+async function get(){
+    const response = await fetch("https://jsonplaceholder.typicode.com/users");
+    const data = await response.json();
+    data.map((user) => {console.log(user.name,user.address)})
+}
+get()
+    .then((response) => console.log("API fetched"))
+    .catch((error) => console.error("Error 404"))
+*/
+
+
+//Third way(axios library)
+
+axios
+    .get("https://jsonplaceholder.typicode.com/users")
+    .then((response) => response.data
+        .map((user) => console.log(user.name, user.address)))
+    .catch((error) => console.error("Error 404"))
